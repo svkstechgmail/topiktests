25c23e7270bdcd7f46e92f515f7800c8d94a257e
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-04-03 12:44:56 -0700

    gatsby init

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Document } from "react-pdf";
+
+// markup
+const IndexPage = () => {
+    const [currentQuestion, setCurrentQuestion] = React.useState(1);
+
+    return (
+        <main>
+            <title>Home Page</title>
+        </main>
+    );
+};
+
+export default IndexPage;
